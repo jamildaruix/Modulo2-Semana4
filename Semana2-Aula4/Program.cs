75c5b96726122a511dffdb732a5c9effe55dfde1
Jamil Salim Daruix <jamil.daruix@pixeon.com>
2022-07-04 10:47:52 -0300

    commit

--- Semana2-Aula4/Program.cs
+++ Semana2-Aula4/Program.cs
@@ -7,82 +7,22 @@ namespace Semana2_Aula4
     {
         static void Main(string[] args)
         {
-            //Exemplo Exercicios via console !!!!
-            Console.WriteLine("Nome: ");
-            string nome = Console.ReadLine();
-            Console.WriteLine($"Nome Preenchido {nome}");
-          
-            //Exemplo Aula 01, 02, 03 e 04
-            Funcionario funcionario = new Funcionario();
-            //Preencher o ID do Funcionário
-            //Colocar a Data de Nascimento 
-            funcionario.Nome = "Nome de vocês aqui";
-            funcionario.SalvarNomeFuncionario();
-            //Refatorar ou criar uma nova sobrecarga
-            //Colocar o console.writeline para impressão da tela
-            funcionario.DadosFuncionario("seu nome", "seu sobrenome");
-            funcionario.DadosFuncionario("seu nome", "seu sobrenome", DateTime.Now.AddYears(-37));
+            string mensagem = "OLÁ MUNDO !";
 
-            Console.WriteLine($"Salario sem Reajuste {funcionario.Salario}");
-            Console.WriteLine($"Aplicando Reajuste Salario de 50%");
-            funcionario.ReajusteConvensaoColetiva(50);
-            Console.WriteLine($"Salario com Reajuste {funcionario.Salario}");
-
-            CentroDeCusto centroDeCusto = new CentroDeCusto(1, "TESTE", DateTime.Now, 10M, 2022);
+            try
+            {
+                int valorMensagem = Convert.ToInt32(mensagem);
+            }
+            catch (AccessViolationException ex)
+            {
+                throw;
+            }
+            catch
+            {
+                throw new ("IXI NÃO FUNCIONOU !!! ┏(-_-)┓");
+            }
 
 
-            //try
-            //{
-            //    /*Método gigante pra Trocar de guitarra*/
-            //    int valor = Convert.ToInt32(centroDeCusto.AnoVerbaLiberada);
-            //}
-            //catch (Exception ex)
-            //{
-            //    string textoSobreescrita = centroDeCusto.ToString();
-            //    Console.Error.WriteLine($@"Erro Métod de converter string para inteiro 
-            //                               | {textoSobreescrita}");
-            //    throw ex;
-            //}
-
-            
-
-
-
-            Departamento departamento = new Departamento(centroDeCusto);
-            departamento.Id = 100;
-            departamento.DesricaoDepartamento = "Dev In House";
-            departamento.CentroDeCusto = new Random(50).Next(5000);
-            departamento.FuncionarioNoDepartamento = funcionario;
-            departamento.InserirFuncionarNoDepartamento();
-
-            ///Efetuar Pagamento Sucesso e alterando o valor do TipoFuncioanrio (ENUM)
-            Pagamento pagamentoEfetuadoSucesso = new Pagamento(1500,100, EnumEstado.SP, 
-                                                EnumTipoFuncionario.PJ, 
-                                                DateTime.Now.AddDays(15));
-
-            /*...*/
-
-            pagamentoEfetuadoSucesso.TipoFuncionario = EnumTipoFuncionario.Autonomo;
-            pagamentoEfetuadoSucesso.EfetuarCalculo();
-
-
-            ///Simular o Erro do pagamento
-            Pagamento pagamentoEfetuadoErro = new Pagamento(3000, 5900, EnumEstado.MG,
-                                                EnumTipoFuncionario.PJ,
-                                                DateTime.Now);
-
-            pagamentoEfetuadoErro.TipoFuncionario = EnumTipoFuncionario.PF;
-            pagamentoEfetuadoErro.EfetuarCalculo();
-
-
-            //var violao = new Violao(true, false);
-            //violao.Afinado();
-            //violao.Afinado(dropAfinacao: true, drop2: true);
-
-            //ExemploEstatico.CinquetaCentavosDeZoom();
-
-            //ExemploClasseInstancia exemploClasse = new ExemploClasseInstancia();
-            //exemploClasse.CinquetaCentavosDeZoom();
         }
     }
 
